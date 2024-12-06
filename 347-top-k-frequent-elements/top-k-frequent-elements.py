4db7d8809e387ac30af85308aabdc0fb866813f9
Siva Swathi Vasagar <137902881+SivaSwathiVasagar@users.noreply.github.com>
2024-12-06 15:08:23 -0500

    Time: 12 ms (15.78%) | Memory: 23.5 MB (5.96%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,15 +1,38 @@
+# -------------------------------------------------------- #
 # Brute force
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         dict = {}
+#         for num in nums:
+#             if num in dict:
+#                 dict[num] += 1
+#             else:
+#                 dict[num] = 1 
+       
+#         sorted_items = sorted(dict.items(), key = lambda x : x[1], reverse = True)
+#         result = []
+#         for n in sorted_items[:k]:
+#             result.append(n[0])
+#         return result  
+# -------------------------------------------------------- #
+# using counter
+# freq_dict = Counter(arr)
+# top_items = freq_dict.most_common(target)
+# return top_items
+# -------------------------------------------------------- #
+# other techniques - Bucket Sort
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        dict = {}
+        count = {}
+        freq = [[] for n in range(len(nums) + 1)]
         for num in nums:
-            if num in dict:
-                dict[num] += 1
-            else:
-                dict[num] = 1 
-       
-        sorted_items = sorted(dict.items(), key = lambda x : x[1], reverse = True)
-        result = []
-        for n in sorted_items[:k]:
-            result.append(n[0])
-        return result   
+            count[num] = 1 + count.get(num,0)
+        for num,cnt in count.items():
+            freq[cnt].append(num)
+        res = []    
+        for i in range(len(freq) - 1,0,-1):
+            for num in freq[i]:
+                res.append(num) 
+                if len(res) == k:
+                    return res
+
