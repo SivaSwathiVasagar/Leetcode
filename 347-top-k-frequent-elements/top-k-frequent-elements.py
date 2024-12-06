2a382ee6255f69d2be48bf0de623a339d7e6001d
Siva Swathi Vasagar <137902881+SivaSwathiVasagar@users.noreply.github.com>
2024-12-06 15:09:02 -0500

    Time: 13 ms (13.74%) | Memory: 23.6 MB (5.96%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,3 +1,18 @@
+# other techniques - Bucket Sort
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for n in range(len(nums) + 1)]
+        for num in nums:
+            count[num] = 1 + count.get(num,0)
+        for num,cnt in count.items():
+            freq[cnt].append(num)
+        res = []    
+        for i in range(len(freq) - 1,0,-1):
+            for num in freq[i]:
+                res.append(num) 
+                if len(res) == k:
+                    return res
 # -------------------------------------------------------- #
 # Brute force
 # class Solution:
@@ -20,19 +35,3 @@
 # top_items = freq_dict.most_common(target)
 # return top_items
 # -------------------------------------------------------- #
-# other techniques - Bucket Sort
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for n in range(len(nums) + 1)]
-        for num in nums:
-            count[num] = 1 + count.get(num,0)
-        for num,cnt in count.items():
-            freq[cnt].append(num)
-        res = []    
-        for i in range(len(freq) - 1,0,-1):
-            for num in freq[i]:
-                res.append(num) 
-                if len(res) == k:
-                    return res
-
